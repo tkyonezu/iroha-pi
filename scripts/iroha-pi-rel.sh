c8a9d756691188cce3fe82c044e9a12e78f0371d
Sleepy Senzu <tkyonezu@gmail.com>
2018-01-18 17:53:44 +0900

    Optimize iroha container image

--- scripts/iroha-pi-rel.sh
+++ scripts/iroha-pi-rel.sh
@@ -8,7 +8,6 @@ rm -fr ${PI_HOME}
 LIBS=$(ldd ${IROHA_HOME}/build/bin/irohad | cut -f 2 | cut -d " " -f 3)
 mkdir -p ${PI_HOME}/lib
 cp -H ${LIBS} ${PI_HOME}/lib
-rsync -av /usr/local/lib ${PI_HOME}/lib
 
 cp -r ${IROHA_HOME}/build/bin ${PI_HOME}
 ## cp -r ${IROHA_HOME}/build/test_bin ${PI_HOME}
