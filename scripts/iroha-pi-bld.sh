9682de3e4aa4a5288c9bab2a0da5737c8564ebce
Sleepy Senzu <tkyonezu@gmail.com>
2018-01-19 10:16:53 +0900

    Skip test build for Rasberry Pi3 (= armv7l)

--- scripts/iroha-pi-bld.sh
+++ scripts/iroha-pi-bld.sh
@@ -6,7 +6,12 @@ else
   NUMCORE=$1
 fi
 
-cmake -H. -Bbuild
+TESTING="ON"
+if [ "$(uname -m)" = "armv7l" ]; then
+  TESTING="OFF"
+fi
+
+cmake -H. -Bbuild -DTESTING=${TESTING}
 cmake --build build -- -j${NUMCORE}
 
 exit 0
