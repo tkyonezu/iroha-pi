a0858584e5c9ec31a6b84dbe0276e67130c88f7c
Sleepy Senzu <tkyonezu@gmail.com>
2018-05-20 23:17:38 +0900

    Bugfixing only armv7l and TESTING=OFF of VirtualBox environment.

--- scripts/iroha-bld.sh
+++ scripts/iroha-bld.sh
@@ -11,15 +11,21 @@ else
   NUMCORE=$1
 fi
 
+PRODUCT_NAME=$(sudo dmidecode -s system-product-name | sed 's/\tProduct Name: //')
+
 TESTING="ON"
 if [ "$(uname -m)" = "armv7l" ]; then
+  TESTING="OFF"
+elif [ "${PRODUCT_NAME}" = "VirtualBox" ]; then
   TESTING="OFF"
 fi
 
 # Bug fix by implicit cast for std::max function.
 # 2018/05/19 by Takeshi Yonezu
-sed -i '/std::max/s/, pass_phrase/, (unsigned long)pass_phrase/' \
-  libs/crypto/keys_manager_impl.cpp
+if [ "$(uname -m)" = "armv7l" ]; then
+  sed -i '/std::max/s/, pass_phrase/, (unsigned long)pass_phrase/' \
+    libs/crypto/keys_manager_impl.cpp
+fi
 
 cmake -H. -Bbuild -DTESTING=${TESTING}
 cmake --build build -- -j${NUMCORE}
