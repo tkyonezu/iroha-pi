3119c7c61544c7fa999cab2b6b2eba08472ff31f
Sleepy Senzu <tkyonezu@gmail.com>
2018-07-10 17:46:51 +0900

    Support Darwin/x86_64 (MacOS) and Linux/x86_64

--- example/node4/logs4.sh
+++ example/node4/logs4.sh
@@ -6,9 +6,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-n=1
+if [[ "$(uname -s)" == "Darwin"  && "$(uname -m)" == "x86_64" ]]; then
+  SYST="MacOS"
+elif [[ "$(uname -s)" == "Linux" && "$(uname -m)" == "x86_64" ]]; then
+  SYST="Linux"
+else
+  echo "($(uname -s)/$(uname -m)) Doesn't Support."
+  exit 1
+fi
+
+
+if [ "${SYT}" == "MacOS" ]; then
+  TERM="/dev/ttys00"
+  n=$(tty | sed '|/dev/ttys||')
+  n=$((n+=0))
+elif [ "${SYST}" == "Linux" ]; then
+  TERM="/dev/pts/"
+  n=$(tty | sed 's|/dev/pts/||')
+fi
 
 for i in $(seq 4); do
-  docker logs -f iroha-node${i} >/dev/ttys00${n} &
-  ((n++))
+  docker logs -f iroha-node${i} >${TERM}$((n + i - 1)) &
 done
