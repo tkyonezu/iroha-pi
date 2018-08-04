3ff7cc1422813569c2ac9c956ed604489c271ffd
Sleepy Senzu <tkyonezu@gmail.com>
2018-08-04 21:56:09 +0900

    Reduce libraries.

--- scripts/iroha-rel.sh
+++ scripts/iroha-rel.sh
@@ -6,13 +6,16 @@
 #
 
 IROHA_HOME=/opt/iroha
-PI_HOME=docker/iroha
+PI_HOME=$(pwd)/docker/iroha
 
 rm -fr ${PI_HOME}
 
-LIBS=$(ldd ${IROHA_HOME}/build/bin/irohad | cut -f 2 | cut -d " " -f 3)
+LIBS=$(ldd ${IROHA_HOME}/build/bin/irohad | cut -f2 | cut -d" " -f3)
 mkdir -p ${PI_HOME}/lib
 cp -H ${LIBS} ${PI_HOME}/lib
+
+cd ${PI_HOME}/lib
+rm -f libc.so.* libdl.so.* libgcc_s.so.* libm.so.* libpthread.so.* librt.so.* libstdc++.so.* libz.so.*
 
 cp -r ${IROHA_HOME}/build/bin ${PI_HOME}
 ## cp -r ${IROHA_HOME}/build/test_bin ${PI_HOME}
