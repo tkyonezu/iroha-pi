7aa26675878e3f9a67d0b042389f10eda63d5257
Sleepy Senzu <tkyonezu@gmail.com>
2019-01-11 00:00:13 +0900

    Status check change for Iroha 1.0.0_rc2

--- example/multi-node/setup-iroha-wallet.sh
+++ example/multi-node/setup-iroha-wallet.sh
@@ -29,8 +29,16 @@ ${IROHA_PORT}
 EOF
 
   hash=$(cat /tmp/hash| grep hash | cut -d' ' -f4)
-  while st $hash | grep -q "A problem"; do
-   sleep 1
+  st $hash >/tmp/st
+  while ! cat /tmp/st | grep -q "successfully committed"; do
+    if cat /tmp/st | grep -q "error"; then
+      echo
+      cat /tmp/st | grep "error" | sed 's/^>.*: //'
+      break
+    fi
+    sleep 1
+    echo -n "."
+    st $hash >/tmp/st
   done
 }
 
