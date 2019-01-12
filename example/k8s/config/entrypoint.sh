4c747ebd394336698e3d6b079689a4366ee3b47e
Sleepy Senzu <tkyonezu@gmail.com>
2019-01-13 08:17:13 +0900

    Raspberry Pi, wait until PostgreSQL is stabilized

--- example/k8s/config/entrypoint.sh
+++ example/k8s/config/entrypoint.sh
@@ -19,6 +19,11 @@ PG_PORT=$(cat config/${IROHA_CONF} | grep pg_opt | sed -e 's/^.*port=//' -e 's/ 
 
 /opt/iroha/bin/wait-for-it.sh -h ${PG_HOST} -p ${PG_PORT} -t 60 -- true
 
+# Raspberry Pi, Wait until PostgreSQL is stabilized
+if [ "$(uname -m)" = "armv7l" ]; then
+  sleep 20
+fi
+
 /opt/iroha/bin/irohad --config config/${IROHA_CONF} \
   --genesis_block config/genesis.block \
   --keypair_name config/${IROHA_NODE} \
