a68ec4be84619ce4fbb319ceb35dec97bccff0c7
Sleepy Senzu <tkyonezu@gmail.com>
2019-01-24 02:18:15 +0900

    Remove redundant parentheses.

--- example/multi-node/bin/iblock.sh
+++ example/multi-node/bin/iblock.sh
@@ -10,9 +10,9 @@ if [ "$(hostname)" = "iroha0" ]; then
   for i in $(seq 3); do
     echo ">>> iroha${i}"
 
-    ssh iroha${i} "(cd ~/github.com/tkyonezu/iroha-pi/example/multi-node; \
+    ssh iroha${i} "cd ~/github.com/tkyonezu/iroha-pi/example/multi-node; \
       echo \"$ ls -l block_store\"; \
-      ls -l block_store)"
+      ls -l block_store"
   done
 fi
 
