11dbce7370321d705aa9c4a0b9110b081e48ec48
Sleepy Senzu <tkyonezu@gmail.com>
2019-01-26 22:19:44 +0900

    Added function to restart with node specification.

--- example/multi-node/bin/irestart.sh
+++ example/multi-node/bin/irestart.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-bash ~/bin/istop.sh
-
-sleep 3
-
-bash ~/bin/istart.sh
+if [ $# -gt 0 ]; then
+  echo "$ ssh $1 bash ~/bin/irestart.sh"
+  ssh $1 bash ~/bin/irestart.sh
+else
+  bash ~/bin/istop.sh
+  sleep 3
+  bash ~/bin/istart.sh
+fi
 
 exit 0
