60710675802a784a1f4c85926559a1be271ea3a0
Sleepy Senzu <tkyonezu@gmail.com>
2019-01-28 13:19:53 +0900

    Add to specify block #.

--- example/multi-node/bin/ishow.sh
+++ example/multi-node/bin/ishow.sh
@@ -2,14 +2,24 @@
 
 cd ~/github.com/tkyonezu/iroha-pi/example/multi-node/block_store
 
-for i in *; do
-  if [ "$i" = '*' ]; then
-    echo ">>> block_store: EMPTY <<<"
-    break
-  fi
+if [ $# -eq 0 ]; then
+  for i in *; do
+    if [ "$i" = '*' ]; then
+      echo ">>> block_store: EMPTY <<<"
+      break
+    fi
 
-  echo ">>> $i <<<"
-  python -m json.tool $i
-done | more
+    echo ">>> $i <<<"
+    python -m json.tool $i
+  done | more
+else
+  if [ -f *$1 ]; then
+    echo ">>> *$1 <<<"
+    python -m json.tool *$1
+  else
+    echo "block: $1 not found."
+    exit 1
+  fi
+fi
 
 exit 0
