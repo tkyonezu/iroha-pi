6b6097a4baec167c2ea0f82c83df809664bb0480
Sleepy Senzu <tkyonezu@gmail.com>
2019-04-18 07:25:12 +0900

    Add IROHA_GENESIS environment variable and check genesis.block exist or not.

--- docker/rel/entrypoint.sh
+++ docker/rel/entrypoint.sh
@@ -4,22 +4,32 @@
 # All Rights Reserved.
 
 IROHA_CONF=${IROHA_CONF:-iroha.conf}
-IROHA_NODEKEY=${IROHA_NODEKEY:-node0}
 IROHA_BLOCK=$(cat config/${IROHA_CONF} | grep block_store_path |
   sed -e 's/^.*: "//' -e 's/".*$//')
+IROHA_GENESIS~${IROHA_GETNESIS:-genesis.block}
+IROHA_NODEKEY=${IROHA_NODEKEY:-node0}
 
 PG_HOST=$(cat config/${IROHA_CONF} | grep pg_opt | sed -e 's/^.*host=//' -e 's/ .*//')
 PG_PORT=$(cat config/${IROHA_CONF} | grep pg_opt | sed -e 's/^.*port=//' -e 's/ .*//')
 
-/opt/iroha/bin/wait-for-it.sh -h ${PG_HOST} -p ${PG_PORT} -t 60 -- true
+${IROHA_HOME}/bin/wait-for-it.sh -h ${PG_HOST} -p ${PG_PORT} -t 60 -- true
 
 # Raspberry Pi, Wait until PostgreSQL is stabilized
 if [ "$(uname -m)" = "armv7l" ]; then
   sleep 30
 fi
 
-cd config
+cd ${IROHA_HOME}/config
 
-echo "$ irohad --genesis_block genesis.block --config ${IROHA_CONF} --keypair_name ${IROHA_NODEKEY}"
+if [ -f ${IROHA_BLOCK}0000000000000001 ]; then
+  echo "$ irohad --config ${IROHA_CONF} --keypair_name ${IROHA_NODEKEY} --verbosity=${VERBOSITY}"
 
-irohad --genesis_block genesis.block --config ${IROHA_CONF} --keypair_name ${IROHA_NODEKEY}
+  irohad --config ${IROHA_CONF} \
+    --keypair_name ${IROHA_NODEKEY}
+else
+  echo "$ irohad --config ${IROHA_CONF} --genesis_block ${IROHA_GENESIS} --keypair_name ${IROHA_NODEKEY} --verbosity=${VERBOSITY}"
+
+  irohad --config ${IROHA_CONF} \
+    --genesis_block ${IROHA_GENESIS} \
+    --keypair_name ${IROHA_NODEKEY}
+fi
