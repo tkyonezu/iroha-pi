7b6ac533f3caf87ca6a10aff38cb97943236ecce
Sleepy Senzu <tkyonezu@gmail.com>
2019-04-18 10:36:01 +0900

    Add env for GENESIS_BLOCK

--- docker/rel/entrypoint.sh
+++ docker/rel/entrypoint.sh
@@ -6,7 +6,7 @@
 IROHA_CONF=${IROHA_CONF:-iroha.conf}
 IROHA_BLOCK=$(cat config/${IROHA_CONF} | grep block_store_path |
   sed -e 's/^.*: "//' -e 's/".*$//')
-IROHA_GENESIS=${IROHA_GETNESIS:-genesis.block}
+IROHA_GENESIS=${IROHA_GENESIS:-genesis.block}
 IROHA_NODEKEY=${IROHA_NODEKEY:-node0}
 
 PG_HOST=$(cat config/${IROHA_CONF} | grep pg_opt | sed -e 's/^.*host=//' -e 's/ .*//')
@@ -21,13 +21,13 @@ fi
 
 cd ${IROHA_HOME}/config
 
-if [ -f ${IROHA_BLOCK}0000000000000001 ]; then
-  echo "$ irohad --config ${IROHA_CONF} --keypair_name ${IROHA_NODEKEY} --verbosity=${VERBOSITY}"
+if [ -f ${IROHA_BLOCK}0000000000000002 ]; then
+  echo "$ irohad --config ${IROHA_CONF} --keypair_name ${IROHA_NODEKEY}"
 
   irohad --config ${IROHA_CONF} \
     --keypair_name ${IROHA_NODEKEY}
 else
-  echo "$ irohad --config ${IROHA_CONF} --genesis_block ${IROHA_GENESIS} --keypair_name ${IROHA_NODEKEY} --verbosity=${VERBOSITY}"
+  echo "$ irohad --config ${IROHA_CONF} --genesis_block ${IROHA_GENESIS} --keypair_name ${IROHA_NODEKEY}"
 
   irohad --config ${IROHA_CONF} \
     --genesis_block ${IROHA_GENESIS} \
