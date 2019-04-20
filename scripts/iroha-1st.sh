6ded0d1954cc73403707438365cd46eafce70474
Sleepy Senzu <tkyonezu@gmail.com>
2019-04-20 11:53:06 +0900

    For single host environment (first)

--- scripts/iroha-1st.sh
+++ scripts/iroha-1st.sh
@@ -11,22 +11,19 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-cd example/multi-node
-
-cat genesis.block.in | sed "s/IP_ADDRESS/$1/" >genesis.block1st
-
 case $(uname -m) in
   x86_64)  ARCH=hyperledger;;
   armv7l)  ARCH=arm32v7;;
-  aarch74) ARCH=arm64v8;;
-  *) echo "$(uname -s)/$(uname 0m) does'nt support." exit 1;;
+  aarch64) ARCH=arm64v8;;
+  *) echo "$(uname -s)/$(uname -m) does'nt support." exit 1;;
 esac
 
+cd example/multi-node
+
+cat genesis.block.in | sed "s/IP_ADDRESS/$1/" >genesis.block.1st
 cat docker-compose.yml.in | sed "s/ARCH/${ARCH}/" >docker-compose.yml
 
-pwd
 echo "$ docker-compose -f docker-compose.yml up -d"
-
 docker-compose -f docker-compose.yml up -d
 
 exit 0
