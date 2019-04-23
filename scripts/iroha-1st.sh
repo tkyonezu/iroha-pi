8f9aec928c98592c6699d16abaa1a7898549b330
Sleepy Senzu <tkyonezu@gmail.com>
2019-04-23 11:27:24 +0900

    Add to support down operation.

--- scripts/iroha-1st.sh
+++ scripts/iroha-1st.sh
@@ -6,27 +6,35 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-if [ $# -lt 2 ]; then
-  echo "Usage: iroha-1st.sh <ip-address> <node-name>"
-  exit 1
+if [ $# -eq 1 -a $1 = "down" ]; then
+  cd example/multi-node
+
+  echo "$ docker-compose -f docker-compose.yml down"
+  docker-compose -f docker-compose.yml down
+
+  echo "$ docker volume prune -f"
+  docker volume prune -f
+
+  exit 0
 fi
 
-case $(uname -m) in
-  x86_64)  ARCH=hyperledger;;
-  armv7l)  ARCH=arm32v7;;
-  aarch64) ARCH=arm64v8;;
-  *) echo "$(uname -s)/$(uname -m) does'nt support." exit 1;;
-esac
-
-IMAGE=$(cat .env | grep IROHA_IMG | sed 's/IROHA_IMG=//')
+if [ $# -lt 1 ]; then
+  echo "Usage: iroha-1st.sh <ip-address> [ <node-name> ]"
+  exit 1
+fi
 
 cd example/multi-node
 
 cat genesis.block.in | sed "s/IP_ADDRESS/$1/" >genesis.block.1st
 
+if [ $# -ge 2 ]; then
+  IROHA_NODEKEY=$2
+else
+  IROHA_NODEKEY=node0
+fi
+
 cat docker-compose.yml.in |
-  sed -e "s|IMAGE: .*|image: ${ARCH}/${IMAGE}|" \
-      -e "s/IROHA_NODEKEY=.*/IROHA_NODEKEY=$2/" >docker-compose.yml
+  sed "s/IROHA_NODEKEY=.*/IROHA_NODEKEY=${IROHA_NODEKEY}/" >docker-compose.yml
 
 echo "$ docker-compose -f docker-compose.yml up -d"
 docker-compose -f docker-compose.yml up -d
