d6f7ec0e14473ff5270ead450485a19750aaa8e9
Sleepy Senzu <tkyonezu@gmail.com>
2019-04-25 10:38:33 +0900

    Add help message

--- scripts/iroha-addpeer.sh
+++ scripts/iroha-addpeer.sh
@@ -6,6 +6,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+if [ $# -eq 0 ]; then
+  echo "Usage: iroha-addpeer <node_name>"
+  echo "       iroha-addpeer [ down | restart ]"
+  exit 0
+fi
+
 if [[ $# -eq 1 && ("$1" = "down" || "$1" = "restart")]]; then
   cd example/multi-node
 
@@ -17,16 +23,10 @@ fi
 
 cd example/multi-node
 
-if [ $# -eq 1 -a  "$1" = "clean" ]; then
+if [ -f block_store/0000000000000001 ]; then
   rm -f block_store/0*
 
-  exit 0
-fi
-
-if [ -f block_store/0000000000000001 ]; then
-
-  echo ">> ERROR: old genesis_block exist!!"
-  exit 1
+  echo ">> WARN: clear old genesis_block!!"
 fi
 
 if [ $# -ge 1 ]; then
