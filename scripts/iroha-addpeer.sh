19180c971d35076dae421ad48bebcd3c79c91bb8
Sleepy Senzu <tkyonezu@gmail.com>
2019-05-09 08:47:35 +0900

    Add more functions.

--- scripts/iroha-addpeer.sh
+++ scripts/iroha-addpeer.sh
@@ -6,28 +6,37 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-if [ $# -eq 0 ]; then
+function usage {
   echo "Usage: iroha-addpeer <node_name>"
-  echo "       iroha-addpeer [ down | restart ]"
+  echo "       iroha-addpeer [ restart | logs | down | clean ]"
   exit 0
+}
+
+if [ $# -eq 0 ]; then
+  usage
 fi
 
-if [[ $# -eq 1 && ("$1" = "down" || "$1" = "restart")]]; then
-  cd example/multi-node
+case "$1" in
+  logs)
+    echo "$ docker logs -f iroha_node_1 &"
+    docker logs -f iroha_node_1 &
+    exit 0;;
+  restart|down)
+    cd example/multi-node
 
-  echo "$ docker-compose -f docker-compose.yml $1"
-  docker-compose -f docker-compose.yml $1
+    echo "$ docker-compose -f docker-compose.yml $1"
+    docker-compose -f docker-compose.yml $1
 
-  exit 0
-fi
+    exit 0;;
+  clean)
+    echo "$ rm -f example/multi-node/block_store/0*"
+    rm -f example/multi-node/block_store/0*
+    exit 0;;
+  *)
+    usage;;
+esac
 
 cd example/multi-node
-
-if [ $# -ge 1 ]; then
-  IROHA_NODEKEY=$1
-else
-  IROHA_NODEKEY=node0
-fi
 
 cat docker-compose.yml.in |
   sed -e "s/IROHA_NODEKEY=.*/IROHA_NODEKEY=${IROHA_NODEKEY}/" >docker-compose.yml
