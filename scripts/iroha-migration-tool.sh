4ae1429e872a93354eabf66c6ef60dc8860e44c3
Takeshi Yonezu <tkyonezu@gmail.com>
2021-10-25 16:59:53 +0900

    Add container name option

--- scripts/iroha-migration-tool.sh
+++ scripts/iroha-migration-tool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -it --rm --entrypoint iroha-migration-tool \
+docker run -it --rm --name iroha-node-1 --entrypoint iroha-migration-tool \
   -v $(pwd)/example/node4/iroha1/block_store:/tmp/block_store \
   -v $(pwd)/example/node4/iroha1/wsv:/tmp/wsv \
   hyperledger/iroha-pi \
