80ce503c0bb9fc96ac29672d0303f1ae2fe0c910
Takeshi Yonezu <tkyonezu@gmail.com>
2021-11-15 22:13:29 +0900

    command name rename from iroha-migration-tool to iroha-migrate.

--- scripts/iroha-migration-tool.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm --name iroha-node-1 --entrypoint iroha-migration-tool \
-  -v $(pwd)/example/node4/iroha1/block_store:/tmp/block_store \
-  -v $(pwd)/example/node4/iroha1/wsv:/tmp/wsv \
-  hyperledger/iroha-pi \
-    --block_store_path /tmp/block_store --rocksdb_path /tmp/wsv
-
-exit 0
