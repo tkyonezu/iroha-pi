a0c9803a566fac0165c1fc924b9f9eef02fdb003
Takeshi Yonezu <tkyonezu@gmail.com>
2022-12-19 13:47:31 +0900

    Added option for interactive mode execution with argument "-i"

--- example/configs/client_cli/setup-iroha-wallet.sh
+++ example/configs/client_cli/setup-iroha-wallet.sh
@@ -7,40 +7,46 @@ function cli ()
   ${CLI} $*
 }
 
-echo '$ iroha_client_cli domain register --id "iroha"'
-read junk
+function msg()
+{
+  echo "$1"
+  if [[ ${INTERACTIVE} == 1 ]]; then
+    echo -n ">>> Press ENTER to continue: "
+    read junk
+  fi
+}
+
+INTERACTIVE=0
+
+if [[ $# > 0  && "$1" == "-i" ]]; then
+  INTERACTIVE=1
+fi
+
+msg '$ iroha_client_cli domain register --id "iroha"'
 cli domain register --id "iroha"
 
-echo '$ iroha_client_cli account register --id "alice@iroha" --key "ed01207233bfc89dcbd68c19fde6ce6158225298ec1131b6a130d1aeb454c1ab5183c0"'
-read junk
+msg '$ iroha_client_cli account register --id "alice@iroha" --key "ed01207233bfc89dcbd68c19fde6ce6158225298ec1131b6a130d1aeb454c1ab5183c0"'
 cli account register --id "alice@iroha" --key "ed01207233bfc89dcbd68c19fde6ce6158225298ec1131b6a130d1aeb454c1ab5183c0"
 
-echo '$ iroha_client_cli account register --id "bob@iroha" --key "ed01207233bfc89dcbd68c19fde6ce6158225298ec1131b6a130d1aeb454c1ab5183c0"'
-read junk
+msg '$ iroha_client_cli account register --id "bob@iroha" --key "ed01207233bfc89dcbd68c19fde6ce6158225298ec1131b6a130d1aeb454c1ab5183c0"'
 cli account register --id "bob@iroha" --key "ed01207233bfc89dcbd68c19fde6ce6158225298ec1131b6a130d1aeb454c1ab5183c0"
 
-echo '$ iroha_client_cli asset register --id "point#iroha" --value-type "Quantity"'
-read junk
+msg '$ iroha_client_cli asset register --id "point#iroha" --value-type "Quantity"'
 cli asset register --id "point#iroha" --value-type "Quantity"
 
-echo '$ iroha_client_cli asset mint --account "alice@iroha" --asset "point#iroha" --quantity 5000'
-read junk
+msg '$ iroha_client_cli asset mint --account "alice@iroha" --asset "point#iroha" --quantity 5000'
 cli asset mint --account "alice@iroha" --asset "point#iroha" --quantity 5000
 
-echo '$ iroha_client_cli asset mint --account "bob@iroha" --asset "point#iroha" --quantity 5000'
-read junk
+msg '$ iroha_client_cli asset mint --account "bob@iroha" --asset "point#iroha" --quantity 5000'
 cli asset mint --account "bob@iroha" --asset "point#iroha" --quantity 5000
 
-## echo '$ iroha_client_cli  account grant --id "bob@iroha" --permission perms.json'
-## read junk
+## msg '$ iroha_client_cli  account grant --id "bob@iroha" --permission perms.json'
 ## cli account grant --id "bob@iroha" --permission perms.json
 
-echo '$ iroha_client_cli -c config-bob.json asset transfer --from bob@iroha --to alice@iroha --asset-id point#iroha --quantity 5'
-read junk
+msg '$ iroha_client_cli -c config-bob.json asset transfer --from bob@iroha --to alice@iroha --asset-id point#iroha --quantity 5'
 cli -c config-bob.json asset transfer --from bob@iroha --to alice@iroha --asset-id point#iroha --quantity 5
 
-echo '$ iroha_client_cli -c config-alice.json asset transfer --from alice@iroha --to bob@iroha --asset-id point#iroha --quantity 100'
-read junk
+msg '$ iroha_client_cli -c config-alice.json asset transfer --from alice@iroha --to bob@iroha --asset-id point#iroha --quantity 100'
 cli -c config-alice.json asset transfer --from alice@iroha --to bob@iroha --asset-id point#iroha --quantity 100
 
 exit 0
