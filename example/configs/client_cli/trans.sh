9ae3362e1192156f8352157a0cef3e110f7fbe39
Takeshi Yonezu <tkyonezu@gmail.com>
2022-12-24 00:12:04 +0900

    Add command line option "-v" for verbose mode

--- example/configs/client_cli/trans.sh
+++ example/configs/client_cli/trans.sh
@@ -7,7 +7,14 @@ function cli ()
   ${CLI} $*
 }
 
+if [[ $# > 0 && "$1" == "-v" ]]; then
+  VERBOSE=1
+fi
+
 for i in $(seq 20); do
+  if [[ ${VERBOSE} == 1 ]]; then
+    echo "$ iroha_client_cli -c config-bob.json asset transfer --from bob@iroha --to alice@iroha --asset-id point#iroha --quantity 1"
+  fi
   cli -c config-bob.json asset transfer --from bob@iroha --to alice@iroha --asset-id point#iroha --quantity 1
 done
 
